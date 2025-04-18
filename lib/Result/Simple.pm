dfd219b00403b695d299daf7dcdb5f7d322bcc44
kobaken <kentafly88@gmail.com>
2025-04-18 21:59:43 +0900

    Updated SYNOPSIS

--- lib/Result/Simple.pm
+++ lib/Result/Simple.pm
@@ -191,39 +191,42 @@ Result::Simple - A dead simple perl-ish Result like F#, Rust, Go, etc.
     use kura Error   => Dict[message => Str];
     use kura Request => Dict[name => Str, age => Int];
 
-    result_for validate_name => Str, Error;
+    result_for validate_name => Request, Error;
 
     sub validate_name {
-        my $name = shift;
+        my $req = shift;
+        my $name = $req->{name};
         return err({ message => 'No name'}) unless defined $name;
         return err({ message => 'Empty name'}) unless length $name;
         return err({ message => 'Reserved name'}) if $name eq 'root';
-        return ok($name);
+        return ok($req);
     }
 
-    result_for validate_age => Int, Error;
+    result_for validate_age => Request, Error;
 
     sub validate_age {
-        my $age = shift;
+        my $req = shift;
+        my $age = $req->{age};
         return err({ message => 'No age'}) unless defined $age;
         return err({ message => 'Invalid age'}) unless $age =~ /\A\d+\z/;
         return err({ message => 'Too young age'}) if $age < 18;
-        return ok($age);
+        return ok($req);
     }
 
     result_for validate_req => Request, Error;
 
     sub validate_req {
-        my $args = shift;
+        my $req = shift;
+        my $err;
 
-        # my $name = validate_name($args->{name}); # => Throw error! It requires list context to handle error
-        my ($name, $name_err) = validate_name($args->{name});
-        return err($name_err) if $name_err;
+        # $req = validate_name($req); # => Throw error! It requires list context to handle error
+        ($req, $err) = validate_name($req);
+        return err($err) if $err;
 
-        my ($age, $age_err) = validate_age($args->{age});
-        return err($age_err) if $age_err;
+        ($req, $err) = validate_age($req);
+        return err($err) if $err;
 
-        return ok({ name => $name, age => $age });
+        return ok($req);
     }
 
     my ($req1, $err1) = validate_req({ name => 'taro', age => 42 });
